26dd746c1833e1735fbf095d5fb6cc835d092069
lab314-21@labeng.local <lab314-21@labeng.local>
2020-01-28 15:32:02 +0700

    final

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,3 +1,26 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+    ifstream cheerbook_copy;
+    cheerbook_copy.open("C:\\Users\\lab314-21\\Desktop\\lab9\\lab9-2562-2-Kitiyaparnnn\\cheerbook.txt");
+    string name;
+    cout<<"-------------------- SOTUS ---------------------\n";
+    
+    while(getline(cheerbook_copy,name))
+    {
+        cout<<name<<"\n";
+    }
+
+    cout<<"-------------------- SOTUS ---------------------";
+    cheerbook_copy.close();
+    return 0;
+}
+
+
 //cout << "-------------------- SOTUS ---------------------";
 //cout << textline;
 //cout << "-------------------- SOTUS ---------------------";
